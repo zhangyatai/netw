d7cf02e408752ce93a6d4dae5818ab5c67ff3384
Dario Caruso <darioyzf@gmail.com>
2015-12-02 13:22:49 +0100

    oki

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 var exec=require('promised-exec'),
 verb=require('verbo'),
+waitfor=require('waitfor-promise'),
+getIP = require('external-ip')(),
 Promise = require('promise');
 
 
 
 
+
 module.exports = function(){
+  var Ip=false
+  function getip(){
+    getIP(function (err, ip){
+      if(err){
+        verb(err,'warn','Netw, external ip error')
+      } else if(ip){
+        Ip=ip
+      }
+    })
+  }
+
+  getip();
+
+  var eip=function(){
+    return new Promise(function (resolve, reject) {
+      if(Ip){
+        resolve(Ip)
+      } else{
+        reject('no ip')
+
+      }
+    })
+  }
+
     return new Promise(function (resolve, reject) {
     exec(__dirname+'/network.sh').then(function(data){
       var networking=JSON.parse(data)
-      resolve(networking);
 
+
+waitfor.pre(eip,{
+  time:1000,
+timeout:6000
+}).then(function(answer){
+  networking.externalIp=answer;
+  resolve(networking)
+}).catch(function(err){
+  verb('no external ip','warn','netw')
+  resolve(networking)
+});
 
     }).catch(function(err){
       reject(err)
