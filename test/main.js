ce77b5464c8238300cf6f3e0b179a1852e4083ab
dario <darioyzf@gmail.com>
2016-02-03 00:12:48 +0100

    ehhehe

--- test/main.js
+++ test/main.js
@@ -18,7 +18,7 @@ describe("netw object", function () {
     });
 
     it("contains an array of networks", function () {
-        expect(networking).to.have.property('networks').that.is.an('array');
+        expect(networking).to.be.an('array');
     });
 
 
@@ -26,36 +26,36 @@ describe("netw object", function () {
     describe("network entry", function () {
 
         it("network structure contains interface, type and mac", function () {
-            for (var i = 0; i < networking.networks.length; i++) {
-                expect(networking.networks[i]).to.have.property('interface').to.be.a('string');
-                expect(networking.networks[i]).to.have.property('type').to.be.a('string');
-                expect(networking.networks[i]).to.have.property('mac').to.be.a('string');
+            for (var i = 0; i < networking.length; i++) {
+                expect(networking[i]).to.have.property('interface').to.be.a('string');
+                expect(networking[i]).to.have.property('type').to.be.a('string');
+                expect(networking[i]).to.have.property('mac').to.be.a('string');
                 var options = ['wifi', 'wired'];
-                expect(options).to.include(networking.networks[i].type);
+                expect(options).to.include(networking[i].type);
             }
         });
 
         it("validate type of interface for API compatibility", function () {
             var options = ['wifi', 'wired'];
-            for (var i = 0; i < networking.networks.length; i++) {
-                expect(options).to.include(networking.networks[i].type);
+            for (var i = 0; i < networking.length; i++) {
+                expect(options).to.include(networking[i].type);
             }
         });
 
         describe("wifi property (if wifi device is present on your computer", function () {
 
             it("wifi networks scan", function () {
-                for (var i = 0; i < networking.networks.length; i++) {
-                    if (networking.networks[i].type == 'wifi') {
-                        expect(networking.networks[i]).to.have.property('scan').to.be.an('array');
+                for (var i = 0; i < networking.length; i++) {
+                    if (networking[i].type == 'wifi') {
+                        expect(networking[i]).to.have.property('scan').to.be.an('array');
                     }
                 }
             });
 
             it("check essid if present", function () {
-                for (var i = 0; i < networking.networks.length; i++) {
-                    if (networking.networks[i].type == 'wifi' && networking.networks[i].gateway, networking.networks[i].essid) {
-                        expect(networking.networks[i]).to.have.property('essid').to.be.a('string');
+                for (var i = 0; i < networking.length; i++) {
+                    if (networking[i].type == 'wifi' && networking[i].gateway, networking[i].essid) {
+                        expect(networking[i]).to.have.property('essid').to.be.a('string');
                     }
                 }
             });
