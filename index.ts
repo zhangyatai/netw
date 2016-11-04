14495d9d4198aa3e43cf65e16e3df1e61e03f14a
dario <darioyzf@gmail.com>
2016-11-04 15:51:07 +0100

    up

---
+++ index.ts
@@ -0,0 +1,49 @@
+import * as child_process from "child_process";
+import * as Promise from "bluebird";
+
+interface Scan {
+    essid: string;
+    mac: string;
+    signal: string;
+}
+
+interface Network {
+    type: string;
+    mac: string;
+    interface: string;
+    essid?: string;
+    scan?: Scan[];
+    ip?: string;
+    gateway?: string;
+}
+
+export default function netw() {
+
+    return new Promise<Network[]>(function(resolve, reject) {
+        let callbacked = false;
+        let timo = setTimeout(function() {
+            if (!callbacked) {
+                reject("timeout error");
+            }
+        }, 20000);
+
+        child_process.exec(__dirname + "/../scripts/linux/network.sh",
+            function(error, stdout, stderr) {
+                callbacked = true;
+                clearTimeout(timo);
+                if (error && error !== null) {
+                    reject(error);
+                } else if (stderr) {
+                    reject(stderr);
+                } else {
+                    resolve(JSON.parse(stdout.toString()));
+                }
+
+            });
+
+
+
+    });
+};
+
+
