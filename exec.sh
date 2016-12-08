d4d8c7f9626bf4642996f26e8312ab6618d94947
Dario Caruso <darioyzf@gmail.com>
2016-12-08 17:05:59 +0100

    ok bin

--- exec.sh
+++ exec.sh
@@ -2,10 +2,12 @@
 
 var child_process = require('child_process')
 
-child_process.exec('node '+__dirname+'/exec/index',function(err,stdout,stderr){
+child_process.exec('node '+__dirname+'/exec/index', function(err,stdout,stderr){
 
 if (err) {
 console.log(err)
-} 
+} else {
+    console.log(stdout.relpace(/ /g,''))
+}
 
 })
--- exec/index.ts
+++ exec/index.ts
@@ -1,7 +1,7 @@
 import netw from "../index"
 
-netw().then(function(d){
-        console.log(JSON.stringify(d));
-}).catch(function(err){
+netw().then(function (d) {
+    console.log(JSON.stringify(d));
+}).catch(function (err) {
     console.log(err);
 });
